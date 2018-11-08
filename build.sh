e50fc1aab9a5e6775cd8cfb974aa204d0350fd9b
Sven van der Meer <vdmeer.sven@mykolab.com>
2018-11-08 11:29:31 +0000

    added gw, extended build script

--- build.sh
+++ build.sh
@@ -34,15 +34,21 @@ shopt -s globstar
 help(){ 
     printf "\nusage: build [targets]\n"
     printf "\n  targets:"
-    printf "\n    all       - build all target"
-    printf "\n    clean     - remove all built artifacts"
-    printf "\n    dist      - build distributions (gradle)"
-    printf "\n    help      - build help files"
-    printf "\n    manual    - build manual files"
-    printf "\n    tool      - build SKB Tool (gradle)"
-    printf "\n    site      - buildsite (maven)"
+    printf "\n    all       - build all target in the right order"
+    printf "\n    clean     - remove all built artifacts (gradle, maven)"
+    printf "\n    dist      - build framework artifacts and then distributions (gradle)"
+    printf "\n    mkdirs    - create required directories to run the framework"
+    printf "\n    tool      - build SKB Framework Tool (gradle)"
+    printf "\n    site      - build the Maven site (maven)"
+    printf "\n"
+    printf "\n Requirements:"
+    printf "\n - gradle          - to build the Java tool and the distributions"
+    printf "\n - JDK8            - to build the tool"
+    printf "\n - Apache Maven    - to build the site"
+    printf "\n - asciidoctor     - to build some targets for the manual"
+    printf "\n - asciidoctor-pdf - to build the PDF manual"
+    printf "\n Requirements are not tested, build will simply fail"
     printf "\n\n"
-
 }
 
 if [[ -z "${1:-}" ]]; then
@@ -50,65 +56,17 @@ if [[ -z "${1:-}" ]]; then
     exit 1
 fi
 
-HAVE_GRADLE=false
-HAVE_MAVEN=false
-HAVE_ADOC=false
-HAVE_ADOC_PDF=false
-
-if [[ -x gradlew ]]; then
-    printf "%s\n" "found gradlew, should work"
-    HAVE_GRADLE=true
-else
-    printf "%s\n" "did not find gradle version >4, cannot build distribution"
-fi
-
-if [[ $(command -v mvn) ]]; then
-    printf "%s %s\n" "found maven" "$(mvn -v | grep "Apache Maven")"
-    HAVE_MAVEN=true
-else
-    printf "%s\n" "did not find maven (required for site), cannot build distribution"
-fi
-if [[ $(command -v asciidoctor) ]]; then
-    printf "%s %s\n" "found maven" "$(asciidoctor -v | grep "Asciidoctor")"
-    HAVE_ADOC=true
-else
-    printf "%s\n" "did not find asciidoctor (required for manual), cannot build distribution"
-fi
-
-if [[ $(command -v asciidoctor-pdf) ]]; then
-    printf "%s %s\n" "found maven" "$(asciidoctor-pdf -v | grep "Asciidoctor")"
-    HAVE_ADOC_PDF=true
-else
-    printf "%s\n" "did not find asciidoctor-pdf (required for manual), cannot build distribution"
-fi
-
-if [[ $HAVE_GRADLE == false ]]; then
-    printf "%s\n" "cannot build distro, no gradle"
-elif [[ $HAVE_MAVEN == false ]]; then
-    printf "%s\n" "cannot build distro, no maven"
-elif [[ $HAVE_ADOC == false ]]; then
-    printf "%s\n" "cannot build distro, no asciidoctor"
-elif [[ $HAVE_ADOC_PDF == false ]]; then
-    printf "%s\n" "cannot build distro, no asciidoctor-pdf"
-else
-    printf "%s\n" "everyting found for building distribution"
-fi
-
-printf "\n"
-
+##
+## function: clean - cleans all artifacts
+##
 clean(){ 
     printf "%s\n\n" "clean"
 
-    if [[ -d ./build ]]; then
-        rm -fr ./build
-    fi
-    if [[ -d ./target ]]; then
-        rm -fr ./target
-    fi
+    ./gradlew clean
+    mvn clean
 
-    if [[ -d ./docs ]]; then
-        rm -fr ./docs/*
-    fi
+    (cd tool; ./gradlew clean)
+    (cd tool; mvn clean)
 
     if [[ -d src/main/bash/bin/java ]]; then
         for file in src/main/bash/bin/java/**; do
@@ -142,72 +100,47 @@ clean(){
     printf "\n\n"
 }
 
-gradle_java(){ 
-    if [[ $HAVE_GRADLE == true ]]; then
-        printf "%s\n\n" "building/copying SKB Tool (java)"
-        ./gradlew -c java.settings
-        mkdir -p src/main/bash/bin/java
-        cp build/libs/skb-framework-tool-0.0.0-all.jar src/main/bash/bin/java
-        printf "\n\n"
-    else
-        printf "%s\n\n" "no gradle, cannot build SKB Tool"
-    fi
-}
+##
+## function: tool - builds the SKB Framework Tool (java)
+##
 
-skb_fw_help(){ 
-    src/main/bash/bin/skb-framework -A -e build-help --sq --lq -T debug
-#     src/main/bash/bin/skb-framework -h
-}
-
-skb_fw_manual(){ 
-    if [[ -f src/main/bash/bin/java/skb-framework-tool-0.0.0-all.jar ]]; then
-        src/main/bash/bin/skb-framework -e build-manual --sq --lq -T debug -- -b --src
-        printf "\n\n"
-    else
-        printf "%s\n\n" "no Tool jar, cannot build framework text sources"
-    fi
-
-    src/main/bash/bin/skb-framework -A -e build-manual --sq --lq -T debug -- -b --adoc --text --requested
+fw_tool(){ 
+    printf "%s\n\n" "building/copying SKB Tool (java)"
+    (cd tool; ./gradlew)
+    mkdir -p src/main/bash/bin/java
+    cp tool/build/libs/skb-framework-tool-0.0.0-all.jar src/main/bash/bin/java
     printf "\n\n"
-
-    if [[ $HAVE_ADOC == true ]]; then
-        src/main/bash/bin/skb-framework -A -e build-manual --sq --lq -T debug -- -b --manp --html
-        printf "\n\n"
-    else
-        printf "%s\n\n" "no asciidoctor, cannot build manual: manp, html"
-    fi
-
-    if [[ $HAVE_ADOC_PDF == true ]]; then
-        src/main/bash/bin/skb-framework -A -e build-manual --sq --lq -T debug -- -b --pdf
-        printf "\n\n"
-    else
-        printf "%s\n\n" "no asciidoctor-pdf, cannot build manual: pdf"
-    fi
 }
 
-skb_fw_site(){ 
-    if [[ $HAVE_MAVEN == true ]]; then
-        src/main/bash/bin/skb-framework -A -e build-mvn-site --sq --lq -T debug -- -bt --id fw
-        (cd docs; chmod 644 `find -type f`)
-    else
-        printf "%s\n\n" "no maven found, cannot build site"
-    fi
+##
+## function: distro - builds framework artifacts and distributions
+##
+
+fw_distro(){ 
+    printf "%s\n\n" "building framework distribution artifacts"
+    src/main/bash/bin/skb-framework --all-mode --sq --lq --task-level debug --run-scenario build-fw-distro
+
+    printf "%s\n\n" "building distributions"
+    ./gradlew
+    printf "\n\n"
+    printf "%s\n" "distributions in ./build/distributions"
+    ls -l ./build/distributions
+    printf "\n\n"
 }
 
-distro(){ 
-    if [[ $HAVE_GRADLE == true ]]; then
-        printf "%s\n\n" "building distributions"
-        ./gradlew -c distribution.settings
-        printf "\n\n"
-        printf "%s\n" "distributions in ./build/distributions"
-        ls -l ./build/distributions
-        printf "\n\n"
-    else
-        printf "%s\n\n" "no gradle, cannot build distributions"
-    fi
+##
+## function: site - builds the Maven site
+##
+fw_site(){ 
+    rm docs/*.html docs/*.txt
+    src/main/bash/bin/skb-framework --all-mode --execute-task build-mvn-site --sq --lq --task-level debug -- --build --targets --id fw
+    (cd docs; chmod 644 `find -type f`)
 }
 
-makeDirs(){ 
+##
+## function: mkdirs - create required directories
+##
+fw_mkdirs(){ 
     mkdir -p src/main/bash/man/man1 2> /dev/null
     mkdir -p src/main/bash/doc/manual 2> /dev/null
 }
@@ -218,10 +151,9 @@ while [[ $# -gt 0 ]]; do
         all)
             T_CLEAN=true
             T_DIST=true
-            T_HELP=true
-            T_MANUAL=true
             T_TOOL=true
             T_SITE=true
+            T_MKDIRS=true
             shift
             ;;
         clean)
@@ -232,12 +164,8 @@ while [[ $# -gt 0 ]]; do
             T_DIST=true
             shift
             ;;
-        help)
-            T_HELP=true
-            shift
-            ;;
-        manual)
-            T_MANUAL=true
+        mkdirs)
+            T_MKDIRS=true
             shift
             ;;
         tool)
@@ -268,22 +196,18 @@ if [[ ${T_CLEAN:-} == true ]]; then
     clean
 fi
 if [[ ${T_TOOL:-} == true ]]; then
-    gradle_java
+    fw_tool
 fi
-if [[ ${T_HELP:-} == true ]]; then
-    makeDirs
-    skb_fw_help
-fi
-if [[ ${T_MANUAL:-} == true ]]; then
-    makeDirs
-    skb_fw_manual
-fi
-if [[ ${T_SITE:-} == true ]]; then
-    skb_fw_site
+if [[ ${T_MKDIRS:-} == true ]]; then
+    fw_mkdirs
 fi
 if [[ ${T_DIST:-} == true ]]; then
-    distro
+    fw_distro
 fi
+if [[ ${T_SITE:-} == true ]]; then
+    fw_site
+fi
+
 
 TE=$(date +%s.%N)
 
