b3e3e623bce104db4cc3cf76865add4575951162
Sven van der Meer <vdmeer.sven@mykolab.com>
2018-11-08 14:34:54 +0000

    tool: added list capability

--- tool/src/main/java/de/vandermeer/skb/framework/Tool.java
+++ tool/src/main/java/de/vandermeer/skb/framework/Tool.java
@@ -20,7 +20,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import de.vandermeer.asciilist.AsciiListContext;
+import de.vandermeer.asciilist.itemize.ItemizeList;
+import de.vandermeer.asciilist.itemize.ItemizeListContext;
 import de.vandermeer.asciiparagraph.AP_Context;
 import de.vandermeer.asciiparagraph.AsciiParagraph;
 import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
@@ -34,13 +39,31 @@ import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
  */
 public class Tool {
 
-    /** The paragraph context with all settings. */
-    public static AP_Context CONTEXT_L1 = new AP_Context().setWidth(74).setAlignment(TextAlignment.JUSTIFIED_LEFT)
+    /** The paragraph context Level 1. */
+    public static final AP_Context AP_CONTEXT_L1 = new AP_Context()
+            .setWidth(74)
+            .setAlignment(TextAlignment.JUSTIFIED_LEFT)
             .setTextLeftMargin(4);
 
-    /** The paragraph context with all settings. */
-    public static AP_Context CONTEXT_L2 = new AP_Context().setWidth(70).setAlignment(TextAlignment.JUSTIFIED_LEFT)
-            .setTextLeftMargin(8);
+    /** The paragraph context Level 2. */
+    public static final AP_Context AP_CONTEXT_L2 = new AP_Context()
+            .setWidth(70)
+            .setAlignment(TextAlignment.JUSTIFIED_LEFT)
+            .setTextLeftMargin(8)
+            .setTextBottomMargin(0);
+
+    /** The list context Level 1. */
+    public static final AsciiListContext AL_CONTEXT_L1 = new ItemizeListContext()
+            .setWidth(74)
+            .setAlignment(TextAlignment.JUSTIFIED_LEFT)
+            .setItemMargin(6);
+
+    /** The list context Level 2. */
+    public static final AsciiListContext AL_CONTEXT_L2 = new ItemizeListContext()
+            .setWidth(70)
+            .setAlignment(TextAlignment.JUSTIFIED_LEFT)
+            .setItemMargin(10);
+
 
     /**
      * Public main to start the tool.
@@ -57,6 +80,7 @@ public class Tool {
      * Creates a new ExecS object.
      */
     public Tool() {
+        // empty constructor
     }
 
     /**
@@ -73,48 +97,66 @@ public class Tool {
 
         File file = new File(args[0]);
         if (!file.exists()) {
-            System.err.println("sf: file " + args[0] + " does not exist");
+            System.err.println("tool: file " + args[0] + " does not exist");
             System.err.println(2);
         }
         if (!file.canRead()) {
-            System.err.println("sf: file " + args[0] + " not readable");
+            System.err.println("tool: file " + args[0] + " not readable");
             System.err.println(3);
         }
 
-        AP_Context ctxt = null;
+        String ctxtLevel = null;
         switch (args[1]) {
         case "l1":
-            ctxt = CONTEXT_L1;
+            ctxtLevel = "L1";
             break;
         case "l2":
-            ctxt = CONTEXT_L2;
+            ctxtLevel = "L2";
             break;
         default:
-            System.err.println("sf: unknown paragraph context: " + args[1]);
+            System.err.println("tool: unknown paragraph context: " + args[1]);
             System.err.println(3);
             break;
         }
 
-        String text = new String();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
+        ArrayList<String> items = new ArrayList<>();
+        boolean isText = true;
+        int paraCount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
-                if (line.equals("+")) {
-                    printText(text, ctxt);
-                    text = new String();
-                } else {
-                    text += line + "\n";
+                if (line.equals("+") || line.equals(" +")) {
+                    if (isText == true && paraCount > 0) {
+                        System.out.println("");
+                    }
+                    printText(items, ctxtLevel, isText);
+                    items.clear();
+                    if (isText == true) {
+                        paraCount += 1;
+                    }
+                    isText = true;
+                }
+                else if (line.startsWith("* ")) {
+                    items.add(line.substring("* ".length()));
+                    isText = false;
+                }
+                else {
+                    items.add(line);
+                    isText = true;
                 }
             }
-            br.close();
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
-        printText(text, ctxt);
+        if (isText == true && paraCount > 0) {
+            System.out.println("");
+        }
+        printText(items, ctxtLevel, isText);
 
         return 0;
     }
@@ -122,13 +164,43 @@ public class Tool {
     /**
      * Print text.
      * 
-     * @param text the text to print as paragraph
-     * @param ctxt the context for the paragraph
+     * @param items the items that should be printed
+     * @param ctxtLevel context level, either L1 or L2
+     * @param isText flag for text, if false a list is printed
      */
-    public void printText(String text, AP_Context ctxt) {
-        AsciiParagraph ap = new AsciiParagraph(ctxt);
-        ap.addText(text);
-        System.out.println(ap.render());
+    public void printText(List<String> items, String ctxtLevel, boolean isText) {
+        if (isText == true) {
+            AP_Context ctxt = null;
+            switch(ctxtLevel) {
+                case "L1":
+                    ctxt = AP_CONTEXT_L1;
+                    break;
+                case "L2":
+                default:
+                    ctxt = AP_CONTEXT_L2;
+                    break;
+            }
+            AsciiParagraph ap = new AsciiParagraph(ctxt);
+            ap.addText(items);
+            System.out.println(ap.render());
+        }
+        else {
+            AsciiListContext ctxt = null;
+            switch(ctxtLevel) {
+                case "L1":
+                    ctxt = AL_CONTEXT_L1;
+                    break;
+                case "L2":
+                default:
+                    ctxt = AL_CONTEXT_L2;
+                    break;
+            }
+            ItemizeList al = new ItemizeList((ItemizeListContext) ctxt);
+            for (String s : items) {
+                al.addItem(s);
+            }
+            System.out.println(al.render());
+        }
     }
 
     /**
