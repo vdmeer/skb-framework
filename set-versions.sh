adac4bd70c8c9b5c31ec9ae435fd49c2c7439d8d
Sven van der Meer <vdmeer.sven@mykolab.com>
2019-06-06 01:00:54 +0100

    manual version fix

--- set-versions.sh
+++ set-versions.sh
@@ -39,3 +39,13 @@ RELEASE_VERSION="$(cat src/main/bash/etc/version.txt)"
 cp src/main/bash/etc/version.txt tool/src/main/resources/tool-version.txt
 ant -f ant/build.xml -DmoduleVersion=${RELEASE_VERSION} -DmoduleDir=../
 chmod 644 src/main/bash/**/*.id src/main/bash/**/*.scn tool/src/**/*.java
+
+printf "\n\n"
+printf "- make sure that src/main/bash/etc/version.txt has only 1 line\n"
+printf "- make sure tool/src/main/resources/tool-version.txt has only 1 line\n"
+printf " - README.adoc version is rebuild automatically\n"
+printf " - manual version changes required for\n"
+printf "   -> pom.xml, tool.pom.xml\n"
+printf "   -> src/site/site.xml\n"
+printf "   -> set-version.sh\n"
+printf "\n\n"
